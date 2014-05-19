3c907eaba7b083cfca08eca292e87a31f357563f
Frode Nerbråten <frode@nerbraten.no>
2014-05-19 11:37:17 +0200

    Case insensitive filename matching

--- src/Document.hs
+++ src/Document.hs
@@ -6,6 +6,7 @@ import Data.Aeson
 import GHC.Generics (Generic)
 import Link
 import Data.List
+import Data.Char
 
 data Documents = Documents { document :: [Document] } deriving (Show, Generic)
 data Document = Document { subject :: String, creatorName :: String, origin :: String, fileType :: String, link :: [Link] } deriving (Show, Generic)
@@ -19,17 +20,23 @@ filename doc = baseName ++ suffix
             | filetype `isSuffixOf` baseName = ""
             | otherwise = filetype
 
+lowerCaseFilename :: Document -> String
+lowerCaseFilename = map toLower . filename
+
 uploaded :: Document -> Bool
 uploaded = ("UPLOADED" ==) . origin
 
 filesNotInDocList :: [FilePath] -> [Document] -> [FilePath]
-filesNotInDocList files = (files \\) . map filename
+filesNotInDocList files = deleteFirstsBy equalIgnoreCase files . map filename
+	where equalIgnoreCase file doc = (map toLower file) == (map toLower doc)
 
 docsInList :: [FilePath] -> [Document] -> [Document]
-docsInList files = filter (\d -> filename d `elem` files)
+docsInList files = filter (\d -> lowerCaseFilename d `elem` lowerFiles)
+	where lowerFiles = map (map toLower) files
 
 docsNotInList :: [FilePath] -> [Document] -> [Document]
-docsNotInList files = filter (\d -> filename d `notElem` files)
+docsNotInList files = filter (\d -> lowerCaseFilename d `notElem` lowerFiles)
+	where lowerFiles = map (map toLower) files
 
 
 instance FromJSON Documents
